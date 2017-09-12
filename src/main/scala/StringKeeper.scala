cdcca33f41c2b15113e657b136d6d582d4d92a8c
kbakha <kbakha@gmail.com>
2017-09-12 07:39:16 +0600

    added tests

--- src/main/scala/StringKeeper.scala
+++ src/main/scala/StringKeeper.scala
@@ -3,12 +3,18 @@ package com.akkademy
 import akka.actor.Actor
 import akka.event.Logging
 
+import scala.collection.mutable.Stack
+
 class StringKeeper extends Actor {
-  var string = ""
+  val stringStack = Stack[String]()
   val log = Logging(context.system, this)
 
   override def receive = {
-    case str: String => string = str
+    case str: String => {
+      stringStack.push(str)
+      sender() ! "come on"
+      println(stringStack)
+    }
     case o => log.info("received unknown message")
   }
 
--- src/test/scala/com/akkademy/StringKeeperSpec.scala
+++ src/test/scala/com/akkademy/StringKeeperSpec.scala
@@ -1,18 +1,23 @@
 package com.akkademy
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorRef, ActorSystem}
 import akka.testkit.{TestActor, TestActorRef}
 import org.scalatest.{BeforeAndAfterEach, FunSpecLike, Matchers}
 
 class StringKeeperSpec extends FunSpecLike with Matchers with BeforeAndAfterEach{
-  implicit val actorSystem = ActorSystem
+  implicit val actorSystem = ActorSystem()
 
   describe("given string"){
     it("should place given string to inner collections") {
       val actorRef = TestActorRef(new StringKeeper)
-      actorRef ! "formidable first string"
-      val akkademyDb = actorRef.underlying
-      akkademyDb.
+      val message = "formidable first string"
+      val secondMessage = "second one"
+      actorRef ! message
+      actorRef ! secondMessage
+      val akkademyDb = actorRef.underlyingActor
+      akkademyDb.stringStack.pop should equal (secondMessage)
+      akkademyDb.stringStack.pop should equal (message)
+
     }
   }
 
